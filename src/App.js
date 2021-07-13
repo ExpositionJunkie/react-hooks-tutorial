33581c86090e1f3a5f4ae6a809817ad5bb1a35da
Roz <rosalindavalbrecht@gmail.com>
2021-07-12 20:02:54 -0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "./useForm";
 import { Hello } from "./Hello";
+import { useFetch } from "./useFetch";
 //https://www.youtube.com/watch?v=f687hBjwFcM
+// * useState
+// * useEffect
+// * next: useRef 35:48
 
 const App = () => {
   const [values, handleChange] = useForm({
@@ -10,32 +14,23 @@ const App = () => {
     firstName: "",
   });
 
-  // useEffect(() => {
-  //   const onMouseMove = (e) => {
-  //     console.log(e);
-  //   };
-  //   window.addEventListener("mousemove", onMouseMove);
+  //using initializer function of use state
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
-  //   return () => {
-  //     window.removeEventListener("mousemove", onMouseMove);
-  //   };
-  // }, [values.email]);
-
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   // Can have as many useEffects as you want on your component
-  useEffect(() => {
-    console.log('mount1')
-  }, [])
-
-  useEffect(() => {
-    console.log('mount2')
-  }, [])
-
-  // const [showHello, setShowHello] = useState(true);
-  // const [values2, handleChange2] = useForm({ firstName: "", lastName: ""});
 
   return (
     <div>
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>increment</button>
       {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
       {showHello && <Hello />}
       <input name="email" value={values.email} onChange={handleChange} /> */}
